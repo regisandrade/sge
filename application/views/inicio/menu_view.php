07102d84bbdcab744f99e5af5e61ed4ccf76bdc6
Regis <regisandrade@gmail.com>
2014-03-27 14:54:46 -0300

    Criando o menu separado para melhor administracao

---
+++ application/views/inicio/menu_view.php
@@ -0,0 +1,80 @@
+<!-- Sidebar -->
+<div id="sidebar">
+
+	<div class="sidebar-tabs">
+        <ul class="tabs-nav two-items">
+            <li><a href="#general" title=""><i class="icon-reorder"></i></a></li>
+            <li><a href="#stuff" title=""></a></li>
+        </ul>
+
+        <div id="general">
+
+	        <!-- Sidebar user -->
+	        <div class="sidebar-user widget">
+				<a href="#" title="" class="user"><img src="<?php echo base_url()?>public/img/<?php echo $config->logo;?>" alt="" /></a>
+	        </div>
+	        <!-- /sidebar user -->
+
+		    <!-- Main navigation -->
+	        <ul class="navigation widget">
+	            <li class="active"><a href="index.html" title=""><i class="icon-home"></i>Administração</a></li>
+	            <li><a href="#" title="" class="expand"><i class="icon-reorder"></i>Form elements<strong>3</strong></a>
+	                <ul>
+	                    <li><a href="forms.html" title="">Form components</a></li>
+	                    <li><a href="wysiwyg.html" title="">WYSIWYG editor</a></li>
+	                    <li><a href="form_wizards.html" title="">Wizards</a></li>
+	                </ul>
+	            </li>
+	            <li><a href="#" title="" class="expand"><i class="icon-tasks"></i>Components<strong>4</strong></a>
+	                <ul>
+	                    <li><a href="components.html" title="">Content components</a></li>
+	                    <li><a href="content_grid.html" title="">Content grid</a></li>
+	                    <li><a href="blank.html" title="">Blank page</a></li>
+	                </ul>
+	            </li>
+	            <li><a href="media.html" title=""><i class="icon-picture"></i>Media</a></li>
+	            <li><a href="icons.html" title=""><i class="icon-th"></i>Icons</a></li>
+	            <li><a href="charts.html" title=""><i class="icon-signal"></i>Charts &amp; graphs</a></li>
+	            <li><a href="invoice.html" title=""><i class="icon-copy"></i>Invoice</a></li>
+	            <li><a href="tables.html" title=""><i class="icon-table"></i>Tables</a></li>
+	            <li><a href="#" title="" class="expand"><i class="icon-warning-sign"></i>Error pages<strong>6</strong></a>
+	                <ul>
+	                    <li><a href="403.html" title="">403 page</a></li>
+	                    <li><a href="404.html" title="">404 page</a></li>
+	                    <li><a href="405.html" title="">405 page</a></li>
+	                    <li><a href="500.html" title="">500 page</a></li>
+	                    <li><a href="503.html" title="">503 page</a></li>
+	                    <li><a href="offline.html" title="">Offline page</a></li>
+	                </ul>
+	            </li>
+	            <li><a href="typography.html" title=""><i class="icon-text-height"></i>Typography</a></li>
+	            <li><a href="calendar.html" title=""><i class="icon-calendar"></i>Calendar</a></li>
+	            <li><a href="file_management.html" title=""><i class="icon-cogs"></i>File management</a></li>
+                <li><a href="#" title="" class="expand"><i class="icon-indent-right"></i>Menu levels<strong>3</strong></a>
+	                <ul>
+	                    <li><a href="#" title="">Link</a></li>
+	                    <li><a href="#" title="" class="expand">Link with submenu</a>
+			                <ul>
+			                    <li><a href="#" title="">Lorem ipsum</a></li>
+			                    <li><a href="#" title="">Dolor sit amet</a></li>
+			                </ul>
+	                    </li>
+	                    <li><a href="#" title="">Link</a></li>
+	                </ul>
+                </li>
+	            <li><a href="#" title="" class="expand"><i class="icon-sitemap"></i>Page layouts<strong>3</strong></a>
+	                <ul>
+	                    <li><a href="no_sidebar_tabs.html" title="">No sidebar tabs</a></li>
+	                    <li><a href="no_action_tabs.html" title="">No action tabs</a></li>
+	                    <li><a href="actions_on_top.html" title="">Action tabs on top</a></li>
+	                    <li><a href="no_breadcrumbs.html" title="">No breadcrumbs line</a></li>
+	                </ul>
+	            </li>
+	        </ul>
+	        <!-- /main navigation -->
+
+        </div>
+
+    </div>
+</div>
+<!-- /sidebar -->
